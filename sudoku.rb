1af7bfafb4d694eb8de67a6bc9c8feb46504c972
kaiz16 <realkaiz7@gmail.com>
2020-02-18 17:17:30 +0800

    Outputting board

--- sudoku.rb
+++ sudoku.rb
@@ -4,7 +4,9 @@ class Sudoku
 # something representing a board after
 # your solver has tried to solve it.
 # How you represent your board is up to you!
+  attr_accessor :game_board
   def initialize(board_string)
+    @game_board = board_string
   end
 
   def solve
@@ -15,9 +17,23 @@ class Sudoku
   end
 
   def board
+    return game_board
   end
 
   # Returns a string representing the current state of the board, well formatted for output to the screen. No `puts` here!
   def to_s
+    output = ""
+    game_board.each_char.with_index do |char, index|
+        output += "#{char} "
+        if (index + 1) % 9 == 0
+            output += "\n"
+        end
+    end
+    return output
   end
 end
+
+puzzle = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+
+game = Sudoku.new(puzzle)
+puts game
