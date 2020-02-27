e5cba0561d99269ef30f0bc391c55fe8c80d17be
kaiz16 <realkaiz7@gmail.com>
2020-02-27 18:09:16 +0800

    Update how board represents to screen

--- sudoku.rb
+++ sudoku.rb
@@ -5,8 +5,18 @@ class Sudoku
 # your solver has tried to solve it.
 # How you represent your board is up to you!
   attr_accessor :game_board
+  attr_reader :initial_board
   def initialize(board_string)
-    @game_board = board_string
+    @initial_board = board_string # Saves the original state of the board
+    @game_board = [] # Store the board as 2D array (Easy to modify and print)
+    row = []
+    board_string.each_char.with_index do |cell, index|
+      row << cell.to_i
+      if (index + 1) % 9 == 0
+        @game_board << row
+        row = []
+      end
+    end
   end
 
   def solve
@@ -16,18 +26,38 @@ class Sudoku
   def finished?
   end
 
+  # Returns original state of the board
   def board
-    return game_board
+    initial_board
   end
 
   # Returns a string representing the current state of the board, well formatted for output to the screen. No `puts` here!
   def to_s
-    output = ""
-    game_board.each_char.with_index do |char, index|
-        output += "#{char} "
-        if (index + 1) % 9 == 0
-            output += "\n"
-        end
+=begin
+      -------------------------
+      | - - - | - - - | - - - | 
+      | - - - | - - - | - - - | 
+      | - - - | - - - | - - - | 
+      -------------------------
+      | - - - | - - - | - - - | 
+      | - - - | - - - | - - - | 
+      | - - - | - - - | - - - | 
+      -------------------------
+      | - - - | - - - | - - - | 
+      | - - - | - - - | - - - | 
+      | - - - | - - - | - - - | 
+      -------------------------
+=end
+    output = "-" * 25 + "\n" 
+    game_board.each_with_index do |row, rowIndex|
+      formatter = "| "
+      row.each_with_index do |col, colIndex|
+        col = "-" if col == 0 # Replace 0s with "-"
+        formatter += "#{col} "
+        formatter += "| " if (colIndex + 1) % 3 == 0 
+      end
+      output += formatter + "\n" # new line after each row
+      output += "-" * 25 + "\n" if (rowIndex + 1) % 3 == 0 # Add lines if rowIndex is divisable by 3
     end
     return output
   end
